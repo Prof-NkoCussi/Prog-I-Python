404dd32ce3bdbb219f542f10a15dcd4298d71865
Prof-NkoCussi <cussi.nico@gmail.com>
2026-09-09 14:41:42 +0000

    cartel kiosco

---
+++ 05_cartel.py
@@ -0,0 +1,16 @@
+#Armá un programa que imprima en pantalla el cartel de precios de un kiosco. Tiene que usar al
+#menos 8 print() y verse ordenado.
+
+# Cartel de precios del kiosco
+# Autor: Prof. Nko Cussi
+
+print("=============================")
+print("     KIOSCO USH")
+print("=============================")
+print("Alfajor .............. $ 1200")
+print("Coca 500ml ........... $ 1800")
+print("Chicles .............. $  600")
+print("Agua 500ml ........... $  900")
+print("Papas fritas ......... $ 1500")
+print("=============================")
+print("     ¡Gracias por venir!")
