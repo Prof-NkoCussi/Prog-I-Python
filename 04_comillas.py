a3d1ddd1ae7e3bfd20a4fb929eec1c96e3e41779
Prof-NkoCussi <cussi.nico@gmail.com>
2026-09-09 14:35:36 +0000

    comillas

---
+++ 04_comillas.py
@@ -0,0 +1,7 @@
+#Esta actividad es para descubrir algo probando, no para que te salga bien a la primera.
+# Experimento: qué hacen las comillas.
+
+nombre = "Martín"
+print(nombre)
+print("nombre")
+print(Martín)
