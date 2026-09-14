a716a7f8aed194b7bfb13cc38031977981674bff
Prof-NkoCussi <cussi.nico@gmail.com>
2026-09-14 20:33:41 +0000

    saludos

--- 07_saludo.py
+++ 07_saludo.py
@@ -4,3 +4,9 @@ nombre = input("¿Cómo te llamás?.. :  ")
 print("¡Hola,", nombre)
 print("Bienvenido a tu primer programa.")
 
+"""
+nombre = input("¿Cómo te llamás? ")
+ciudad = input("¿De dónde sos? ")
+print("¡Hola,", nombre + "!")
+print("Así que sos de", ciudad + ". Qué bueno.")
+"""
