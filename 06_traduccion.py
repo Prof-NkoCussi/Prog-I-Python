b20a98e439a90475a6c3f59b704c14e8da002160
Prof-NkoCussi <cussi.nico@gmail.com>
2026-09-14 20:22:35 +0000

    pasar pseudocog a python

---
+++ 06_traduccion.py
@@ -0,0 +1,9 @@
+# Total de una compra
+precio = 1500
+cantidad = 3
+total = precio * cantidad
+print("El total es")
+print(total)
+
+#a) Cambió únicamente mostrar por print. Las tres primeras líneas quedaron idénticas.
+#b) Una sola cosa: el nombre de la función que muestra en pantalla.
