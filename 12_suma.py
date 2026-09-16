c6beb29b3ced99bcbee024d972b3a40166997e92
Prof-NkoCussi <cussi.nico@gmail.com>
2026-09-16 14:22:59 +0000

    suma

---
+++ 12_suma.py
@@ -0,0 +1,14 @@
+"""Programa que pida dos números al usuario y muestre la suma.
+Primer número: 12
+Segundo número: 8
+La suma es 20
+Cuidado: si te muestra 128 en vez de 20, ya sabés cuál es el problema. Arreglalo."""
+
+# Suma de dos números
+
+numero_1 = int(input("Primer número: "))
+numero_2 = int(input("Segundo número: "))
+ 
+suma = numero_1 + numero_2
+ 
+print("La suma es", suma)
