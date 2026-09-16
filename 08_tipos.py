83f9621ac3081585d6fa0bc0cdd1b22719bdfc64
Prof-NkoCussi <cussi.nico@gmail.com>
2026-09-16 12:52:32 +0000

    tipos de datos

---
+++ 08_tipos.py
@@ -0,0 +1,11 @@
+# Detective de tipos de datos
+
+edad = 17
+precio = 2500.50
+nombre = "Sofía"
+aprobado = True
+
+print(type(edad))
+print(type(precio))
+print(type(nombre))
+print(type(aprobado))
