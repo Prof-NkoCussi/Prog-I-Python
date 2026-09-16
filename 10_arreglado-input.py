14aee2d2368b4e672ed158c6d49f19dd7f0ed5dd
Prof-NkoCussi <cussi.nico@gmail.com>
2026-09-16 13:31:53 +0000

    input

---
+++ 10_arreglado-input.py
@@ -0,0 +1,18 @@
+"""Arreglá el programa de la actividad anterior para que funcione:"""
+
+# Ahora sí funciona
+
+edad = int(input("¿Cuántos años tenés? "))
+print(edad)
+
+"""
+| Dato que se pide              | ¿Se usa para cuentas? | ¿Se convierte? ¿Con qué?       |
+|-------------------------------|------------------------|-------------------------------|
+| El nombre del cliente         | No                     | No se convierte               |
+| La cantidad de alfajores      | Sí                     | Sí, con int()                 |
+| El precio de la coca          | Sí                     | Sí, con float()               |
+| La dirección de la casa       | No                     | No se convierte               |
+| La nota de un examen          | Sí                     | Sí, con float() o int()       |
+| El número de teléfono         | No                     | No se convierte               |
+| El saldo de la SUBE           | Sí                     | Sí, con float() o int()       |
+"""
