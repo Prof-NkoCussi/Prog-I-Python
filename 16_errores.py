92dd7069769206ccfd9d2d6f5ee2c90c537a9469
Prof-NkoCussi <cussi.nico@gmail.com>
2026-09-18 14:03:31 +0000

    errores comunes

---
+++ 16_errores.py
@@ -0,0 +1,34 @@
+#Programa A
+Print("Hola")
+
+#Programa B
+nombre = Sofía
+print(nombre)
+
+#Programa C
+precio = input("Precio: ")
+cantidad = input("Cantidad: ")
+print("Total:", precio * cantidad)
+
+#Programa D
+edad = 17
+print("Tu edad es", Edad)
+
+#Programa E
+total = precio * cantidad
+precio = 1500
+cantidad = 2
+print(total)
+
+#Programa F
+print("El total es"
+print("Gracias")
+
+| Programa |   Error     |            Tipo de problema             |
+|----------|-------------|-----------------------------------------|
+| A        | NameError   | Mayúscula en el nombre de la función    |
+| B        | NameError   | Faltan comillas                         |
+| C        | TypeError   | Falta convertir con `int()` o `float()` |
+| D        | NameError   | Mayúscula en el nombre de la variable   |
+| E        | NameError   | Orden de las líneas                     |
+| F        | SyntaxError | Paréntesis sin cerrar                   |
